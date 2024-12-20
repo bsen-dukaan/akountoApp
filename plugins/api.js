fbdaf0190b81af388eb31966f47c764ee82130dc
bsen <biswadevelops@gmail.com>
2024-12-20 15:38:12 +0530

    added kounto api

--- plugins/api.js
+++ plugins/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 class API {
   constructor() {
-    this.baseURL = "https://kounto.dukaaninfra.in/api"; //  "https://api.kounto.ai/api";
+    this.baseURL = "https://api.kounto.ai/api"; //  "https://api.kounto.ai/api";
     if (typeof window !== "undefined") {
       this.token = localStorage.getItem("token");
     } else {
